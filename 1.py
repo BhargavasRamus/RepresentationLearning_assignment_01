7c3573acad7ea9d65065fe9d4762f304d30207d0
Pamula Bhargav Ram <29677592+BhargavasRamus@users.noreply.github.com>
2018-09-16 17:44:35 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
 
+#gettig data from image
+image=ndimage.imread("houses.jpg")
+plt.figure()
+plt.imshow(image)
+x, y, z =image.shape
+X=image.reshape(x*y,z)(dtype=float64)
+
+#inputs
+print("provide the number of clusters:")
+K=int(input())
+print("provide the threshold value:")
+e=float(input())
+
+#initializing centroids
+centeroids=[]
+for i in range K:
+  centeroids.append(X[][])
+
+  
